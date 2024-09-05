d16ecd6440435e58cd410361732a7cc31dc7f028
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-09-05 10:44:57 +0530

    Made login page for govt official, farmer and consumer.

---
+++ app/src/main/java/com/example/krushakbazaar/LoginPageForGovtOfficial.java
@@ -0,0 +1,21 @@
+package com.example.krushakbazaar;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginPageForGovtOfficial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_page_for_govt_official);
+
+
+    }
+}
