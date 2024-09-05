d77454db63b06d766742dd6c713a9fbd098aa77e
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-09-05 10:42:12 +0530

    Made Login page and added few files.

--- app/src/main/java/com/example/krushakbazaar/LoginPage.java
+++ app/src/main/java/com/example/krushakbazaar/LoginPage.java
@@ -1,6 +1,9 @@
 package com.example.krushakbazaar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginPage extends AppCompatActivity {
 
+    private Button backButtonLoginPage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page);
 
+        backButtonLoginPage = findViewById(R.id.back_button_loginpage);
 
+        backButtonLoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginPage.this, IndexPage.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                finish();
+            }
+        });
     }
 }
